a3029a5326eb09adbf9b936694cb6be91485a0bc
Nick Sahm <sahm.nick@gmail.com>
2020-04-22 13:33:42 -0400

    title_screen
    General layout of titlescreen with button functionality. Still need to finalize background artwork

---
+++ Exit.gd
@@ -0,0 +1,8 @@
+extends Button
+
+
+
+
+func _on_Exit_button_up():
+	get_tree().quit()
+	pass # Replace with function body.
---
+++ NewGame.gd
@@ -0,0 +1,10 @@
+extends Button
+
+
+
+
+func _on_NewGame_pressed():
+	
+	get_tree().change_scene("res://DylanSampleScene.tscn")
+	
+	pass # Replace with function body.
