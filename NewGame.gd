9236cb8ec8beaf2ede53131a1ac634abbeaed388
Nick Sahm <sahm.nick@gmail.com>
2020-04-23 15:40:39 -0400

    Menu Screens With Links
    Title Screen
    Game Over Screen
    Victory Screen
    with working buttons

--- NewGame.gd
+++ NewGame.gd
@@ -5,6 +5,6 @@ extends Button
 
 func _on_NewGame_pressed():
 	
-	get_tree().change_scene("res://DylanSampleScene.tscn")
+	get_tree().change_scene("res://TitleScreen.tscn")
 	
 	pass # Replace with function body.
